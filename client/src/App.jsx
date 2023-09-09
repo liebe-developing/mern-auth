28ca533fe6cfdd3e8b257957818917d99cb4e722
ElliotCyber <ali.razmjooei78@gmail.com>
2023-09-09 15:18:38 +0330

    installing react-router-dom and create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Home, Profile, Signin, Signup } from "./pages";
+
 export default function App() {
-  return <h1 className="flex text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
